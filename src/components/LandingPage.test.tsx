193980cd7124df31d69cf64d7783afe08434e849
Wolfgang Zolleis <wzolleis@web.de>
2019-08-22 19:54:12 +0200

    LandingPage mit MapComponent und Einbinden von leaflet.js

---
+++ src/components/LandingPage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import LandingPage from './LandingPage'
+
+describe('landing page tests', () => {
+    it('renders without crash', () => {
+        const wrapper = shallow(<LandingPage/>);
+        expect(wrapper.find('#LandingPage-test')).toHaveLength(1);
+    })
+})
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MapContainer from './MapContainer'
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div id={'LandingPage-test'}>
+                <MapContainer/>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
